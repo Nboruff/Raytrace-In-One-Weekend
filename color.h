254047bde332b9c6dcbd94ea24a12c2a2a5df23a
Nboruff <nboruff86@gmail.com>
2020-05-25 18:55:53 -0400

    Completed All of Section 8: Diffuse Materials

--- color.h
+++ color.h
@@ -13,10 +13,9 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel)
 
     // Divide the color total by the number of samples.
     auto scale = 1.0 / samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
-
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
--- vec3.h
+++ vec3.h
@@ -22,7 +22,7 @@ class vec3{
         vec3& operator+=(const vec3 &v) {
             e[0] += v.e[0];
             e[1] += v.e[1];
-            e[2] += v.e[3];
+            e[2] += v.e[2];
             return *this;
         }
         
@@ -43,6 +43,16 @@ class vec3{
 
         double length_squared() const {
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        }
+
+        inline static vec3 random()
+        {
+            return vec3(random_double(), random_double(), random_double());
+        }
+
+        inline static vec3 random(double min, double max)
+        {
+            return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
         }
 
     public:
@@ -102,6 +112,33 @@ inline vec3 cross(const vec3 &u, const vec3 &v)
 inline vec3 unit_vector(vec3 v)
 {
     return v / v.length();
+}
+
+vec3 random_in_unit_sphere()
+{
+    while (true)
+    {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() >= 1)
+            continue;
+        return p;
+    }
+}
+
+vec3 random_in_unit_vector(){
+    auto a = random_double(0, 2 * pi);
+    auto z = random_double(-1, 1);
+    auto r = sqrt(1 - z * z);
+    return vec3(r * cos(a), r *sin(a), z);
+}
+
+vec3 random_in_hemisphere(const vec3 &normal)
+{
+    vec3 in_unit_sphere = random_in_unit_sphere();
+    if (dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+        return in_unit_sphere;
+    else
+        return -in_unit_sphere;
 }
 
 #endif
