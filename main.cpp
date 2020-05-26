27918415a5d8a0c8e4687a9e0189730d780e225d
Nboruff <nboruff86@gmail.com>
2020-05-26 14:30:16 -0400

    Completed All of Section 9: Metal

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "hittable_list.h"
 #include "sphere.h"
 #include "color.h"
-
+#include "material.h"
 #include <iostream>
 
 double hit_sphere(const point3 &center, double radius, const ray &r)
@@ -27,8 +27,11 @@ color ray_color(const ray& r, const hittable& world, int depth) {
     }
 
     if (world.hit(r, 0.001 , infinity, rec)) {
-        point3 target = rec.p + rec.normal + random_in_unit_vector();
-        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
+        ray scattered;
+        color attenuation;
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+            return attenuation * ray_color(scattered, world, depth-1);
+        return color(0,0,0);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
@@ -47,12 +50,19 @@ int main()
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 
-    hittable_list world;
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1),100));
+hittable_list world;
+
+    world.add(make_shared<sphere>(
+        point3(0,0,-1), 0.5, make_shared<lambertian>(color(0.7, 0.3, 0.3))));
+
+    world.add(make_shared<sphere>(
+        point3(0,-100.5,-1), 100, make_shared<lambertian>(color(0.8, 0.8, 0.0))));
+
+    world.add(make_shared<sphere>(point3(1,0,-1), 0.5, make_shared<metal>(color(.8,.6,.2), 0.0)));
+    world.add(make_shared<sphere>(point3(-1,0,-1), 0.5, make_shared<metal>(color(.8,.8,.8), 0.0)));
 
     camera cam;
-    
+
     for (int j = image_height - 1; j >= 0; --j)
     {
         std::cerr << "\rScanlines remaining: " << j << std::flush;
--- vec3.h
+++ vec3.h
@@ -125,7 +125,7 @@ vec3 random_in_unit_sphere()
     }
 }
 
-vec3 random_in_unit_vector(){
+vec3 random_unit_vector(){
     auto a = random_double(0, 2 * pi);
     auto z = random_double(-1, 1);
     auto r = sqrt(1 - z * z);
@@ -141,4 +141,7 @@ vec3 random_in_hemisphere(const vec3 &normal)
         return -in_unit_sphere;
 }
 
+vec3 reflect(const vec3& v, const vec3& n){
+    return v - 2*dot(v,n)*n;
+}
 #endif
