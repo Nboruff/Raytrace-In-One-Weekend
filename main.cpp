ede6c9c2cc919342f528b6f69de7ed173a1884bc
Nboruff <nboruff86@gmail.com>
2020-05-26 20:21:38 -0400

    Completed All of Section 10: Dielectrics

--- main.cpp
+++ main.cpp
@@ -50,17 +50,13 @@ int main()
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 
-hittable_list world;
+    hittable_list world;
 
-    world.add(make_shared<sphere>(
-        point3(0,0,-1), 0.5, make_shared<lambertian>(color(0.7, 0.3, 0.3))));
-
-    world.add(make_shared<sphere>(
-        point3(0,-100.5,-1), 100, make_shared<lambertian>(color(0.8, 0.8, 0.0))));
-
-    world.add(make_shared<sphere>(point3(1,0,-1), 0.5, make_shared<metal>(color(.8,.6,.2), 0.0)));
-    world.add(make_shared<sphere>(point3(-1,0,-1), 0.5, make_shared<metal>(color(.8,.8,.8), 0.0)));
-
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5, make_shared<lambertian>(color(.1, .2, .5))));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, make_shared<lambertian>(color(.8, .8, 0.))));
+    world.add(make_shared<sphere>(point3(1, 0, -1), 0.5, make_shared<metal>(color(.8, .6, .2), 0.3)));
+    world.add(make_shared<sphere>(point3(-1, 0, -1), 0.5, make_shared<dielectric>(1.5)));
+    world.add(make_shared<sphere>(point3(-1, 0, -1), -0.45, make_shared<dielectric>(1.5)));
     camera cam;
 
     for (int j = image_height - 1; j >= 0; --j)
@@ -68,15 +64,16 @@ hittable_list world;
         std::cerr << "\rScanlines remaining: " << j << std::flush;
         for (int i = 0; i < image_width; ++i)
         {
-            color pixel_color(0,0,0);
-            for(int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width-1);
-                auto v = (j + random_double()) / (image_height-1);
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s)
+            {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
                 pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
-    std::cerr << "\nDone.\n";
+        std::cerr << "\nDone.\n";
 }
